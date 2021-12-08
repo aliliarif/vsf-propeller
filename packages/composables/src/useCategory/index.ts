58b8e516674d8d6b8b363761d5351aa972546105
aalili <a.alili@propel.us>
2021-12-09 00:34:28 +0100

    fix: Product gallery

--- packages/composables/src/useCategory/index.ts
+++ packages/composables/src/useCategory/index.ts
@@ -9,8 +9,6 @@ import type { UseCategorySearchParams as SearchParams } from '../types';
 const params: UseCategoryFactoryParams<Category, SearchParams> = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   categorySearch: async (context: Context, params) => {
-    console.log('Propeller: useCategory.categorySearch', params);
-
     const { data } = await context.$propeller.api.categories();
 
     return data.category.categories || {};
